885ee581bdcb2f086ccbbbafa667d8c79ef2fa04
Andrewx987 <andres.pb.16@gmail.com>
2018-05-11 17:37:59 -0300

    Subiendo proyecto por primera vez

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miPrimeraAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the miPrimeraAppApp
+ */
+angular.module('miPrimeraAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miPrimeraAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the miPrimeraAppApp
+ */
+angular.module('miPrimeraAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
